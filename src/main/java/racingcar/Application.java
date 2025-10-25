2eb2d7b1813eff02c5f679161ec92ce8f31c44d4
choubung <choubung@sookmyung.ac.kr>
2025-10-26 01:44:56 +0900

    refactor(Application): 자동차 입력과 횟수 입력을 메인 메소드에서 분리
    메인 메소드에서는 간결한 입출력만 실행되도록 하기 위해 변경함.
    자동차 이름 입력은 startNewGame() 메소드로 분리함.
    횟수 입력 및 횟수에 대한 예외 처리는 setRounds() 메소드로 분리함.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,10 +5,25 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String[] nameList = Console.readLine().replace(" ", "").split(",");
-        Car.register(nameList);
+        RacingGameService game = startNewGame();
 
         System.out.println("시도할 횟수는 몇 회인가요?");
+        int rounds = setRounds();
+
+        System.out.println("실행 결과");
+        while (rounds-- > 0) {
+            game.printRoundResult();
+        }
+
+        game.printWinners();
+    }
+
+    private static RacingGameService startNewGame() {
+        String[] nameList = Console.readLine().replace(" ", "").split(",");
+        return new RacingGameService(nameList);
+    }
+
+    private static int setRounds() {
         try {
             int rounds = Integer.parseInt(Console.readLine());
 
@@ -16,15 +31,10 @@ public class Application {
                 throw new Exception();
             }
 
-            System.out.println("실행 결과");
-            while (rounds-- > 0) {
-                Car.forward();
-                Car.printRoundResult();
-            }
+            return rounds;
+
         } catch (Exception e) {
             throw new IllegalArgumentException("1 이상의 정수를 입력해야 합니다.");
         }
-
-        Car.printWinners();
     }
 }
