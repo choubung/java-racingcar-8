61ab5616ad5fe82b840be7630c64ab069dd9b264
choubung <choubung@sookmyung.ac.kr>
2025-10-25 23:01:25 +0900

    feat(Car): 자동차 클래스 Car.java 작성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,29 @@
 package racingcar;
 
 public class Car {
+    String name;
+    int distance = 0;
+    Boolean winner = false;
+    static int times;
+
+    public Car() { }
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public static void setTimes(int times) {
+        Car.times = times;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public Boolean getWinner() {
+        return winner;
+    }
 }
