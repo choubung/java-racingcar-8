a88a4eb54b55d01fda3baf1cc7c9b6b85970cb76
choubung <choubung@sookmyung.ac.kr>
2025-10-26 01:40:19 +0900

    refactor(Car,RacingGameService): Car에서 RacingGameService로 책임 분리
    Car 클래스에 자동차 목록 등록, 라운드 결과 출력, 최종 우승자 출력과 같은 게임 관련 로직이 구현되어 있어 단일책임 원칙에 위배된다고 파악함.
    위에 언급된 로직을 RacingGameService로 분리함.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,12 +2,7 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Car {
-    static List<Car> carList = new ArrayList<>();
     String name;
     int distance = 0;
 
@@ -18,60 +13,11 @@ public class Car {
         this.name = name;
     }
 
-    public static void register(String[] nameList) {
-        if (nameList.length == 0) {
-            throw new IllegalArgumentException("자동차는 1대 이상 입력되어야 합니다.");
+    public void forward() {
+        int randomNum = Randoms.pickNumberInRange(0, 9);
+        if (randomNum >= 4) {
+            this.distance++;
         }
-
-        for (String name : nameList) {
-            if (!name.matches("^[a-zA-Z0-9가-힣]*$")) {
-                throw new IllegalArgumentException("이름은 한글, 영문자, 숫자만 사용할 수 있습니다.");
-            }
-
-            if (name.length() > 5) {
-                throw new IllegalArgumentException("자동차의 이름은 5자 이하여야 합니다.");
-            }
-
-            carList.add(new Car(name));
-        }
-    }
-
-    public static void forward() {
-        for (Car car : carList) {
-            int randomNum = Randoms.pickNumberInRange(0, 9);
-            if (randomNum >= 4) {
-                car.distance++;
-            }
-        }
-    }
-
-    public static void printRoundResult() {
-        for (Car car : carList) {
-            StringBuilder result = new StringBuilder();
-            result.append(car.getName()).append(" : ").append("-".repeat(car.getDistance()));
-            System.out.println(result);
-        }
-        System.out.println();
-    }
-
-    public static void printWinners() {
-        List<Car> winners = new ArrayList<>();
-        int maxDistance = carList.stream()
-                .mapToInt(Car::getDistance)
-                .max()
-                .orElse(0);
-
-        for (Car car : carList) {
-            if (car.getDistance() == maxDistance) {
-                winners.add(car);
-            }
-        }
-
-        String winnerNames = winners.stream()
-                .map(Car::getName)
-                .collect(Collectors.joining(", "));
-
-        System.out.println("최종 우승자 : " + winnerNames);
     }
 
     public String getName() {
---
+++ src/main/java/racingcar/RacingGameService.java
@@ -0,0 +1,64 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RacingGameService {
+    List<Car> carList = new ArrayList<>();
+
+    public RacingGameService(String[] nameList) {
+        if (nameList.length == 0) {
+            throw new IllegalArgumentException("자동차는 1대 이상 입력되어야 합니다.");
+        }
+
+        for (String name : nameList) {
+            if (!name.matches("^[a-zA-Z0-9가-힣]*$")) {
+                throw new IllegalArgumentException("이름은 한글, 영문자, 숫자만 사용할 수 있습니다.");
+            }
+
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("자동차의 이름은 5자 이하여야 합니다.");
+            }
+
+            carList.add(new Car(name));
+        }
+    }
+
+    public void playRound(){
+        for (Car car : carList) {
+            car.forward();
+        }
+    }
+
+    public void printRoundResult() {
+        playRound();
+
+        for (Car car : carList) {
+            StringBuilder result = new StringBuilder();
+            result.append(car.getName()).append(" : ").append("-".repeat(car.getDistance()));
+            System.out.println(result);
+        }
+        System.out.println();
+    }
+
+    public void printWinners() {
+        List<Car> winners = new ArrayList<>();
+        int maxDistance = carList.stream()
+                .mapToInt(Car::getDistance)
+                .max()
+                .orElse(0);
+
+        for (Car car : carList) {
+            if (car.getDistance() == maxDistance) {
+                winners.add(car);
+            }
+        }
+
+        String winnerNames = winners.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(", "));
+
+        System.out.println("최종 우승자 : " + winnerNames);
+    }
+}
