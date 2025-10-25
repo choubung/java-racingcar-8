075a8a055e55c15b96a3fcbd533d15bb7ff5c0f4
choubung <choubung@sookmyung.ac.kr>
2025-10-25 23:16:59 +0900

    fix(Application): Scanner 대신 Console을 사용하도록 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,21 +1,19 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String[] nameList = sc.nextLine().split(",");
+        String[] nameList = Console.readLine().split(",");
         List<Car> carList = new ArrayList<>();
         for (String name : nameList) {
             carList.add(new Car(name));
         }
 
         System.out.println("시도할 횟수는 몇 회인가요?");
-        Car.setTimes(sc.nextInt());
+        Car.setTimes(Integer.parseInt(Console.readLine()));
     }
 }
