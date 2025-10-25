06474773c575b26edc9146c5be0a463e5653d17a
choubung <choubung@sookmyung.ac.kr>
2025-10-25 23:02:40 +0900

    feat(Application): 자동차 등록(이름과 경주 횟수) 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] nameList = sc.nextLine().split(",");
+        List<Car> carList = new ArrayList<>();
+        for (String name : nameList) {
+            carList.add(new Car(name));
+        }
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        Car.setTimes(sc.nextInt());
     }
 }
